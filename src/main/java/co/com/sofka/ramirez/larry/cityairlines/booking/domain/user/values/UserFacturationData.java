7a7b852ebb328fe7c2807194b11b464c50908763
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-02-28 15:24:38 -0500

    Create the value object UserFacturationData

---
+++ src/main/java/co/com/sofka/ramirez/larry/cityairlines/booking/domain/user/values/UserFacturationData.java
@@ -0,0 +1,50 @@
+package co.com.sofka.ramirez.larry.cityairlines.booking.domain.user.values;
+
+import co.com.sofka.ramirez.larry.cityairlines.booking.application.generic.ValueObject;
+
+public class UserFacturationData implements ValueObject<UserFacturationData.Props> {
+
+    private final String street;
+    private final String city;
+    private final String country;
+    private final String zipCode;
+
+    public UserFacturationData(String street, String city, String country, String zipCode) {
+        this.street = street;
+        this.city = city;
+        this.country = country;
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public Props value() {
+        return new Props() {
+            @Override
+            public String street() {
+                return street;
+            }
+
+            @Override
+            public String city() {
+                return city;
+            }
+
+            @Override
+            public String country() {
+                return country;
+            }
+
+            @Override
+            public String zipCode() {
+                return zipCode;
+            }
+        };
+    }
+
+    public interface Props {
+        String street();
+        String city();
+        String country();
+        String zipCode();
+    }
+}
