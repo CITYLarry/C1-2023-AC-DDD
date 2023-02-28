c8e1bb2950d313136fba3fea601ff0b41c4966d3
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-02-28 15:33:07 -0500

    Create the value object UserData

---
+++ src/main/java/co/com/sofka/ramirez/larry/cityairlines/booking/domain/user/values/UserData.java
@@ -0,0 +1,42 @@
+package co.com.sofka.ramirez.larry.cityairlines.booking.domain.user.values;
+
+import co.com.sofka.ramirez.larry.cityairlines.booking.application.generic.ValueObject;
+
+public class UserData implements ValueObject<UserData.Props> {
+
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public UserData(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Props value() {
+        return new Props() {
+            @Override
+            public String name() {
+                return name;
+            }
+
+            @Override
+            public String email() {
+                return email;
+            }
+
+            @Override
+            public String password() {
+                return password;
+            }
+        };
+    }
+    
+    public interface Props {
+        String name();
+        String email();
+        String password();
+    }
+}
