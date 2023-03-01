f45c6da1a85482455fca7706c49b0aa18ea41f50
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-02-28 19:47:44 -0500

    Create the Behavior UserBehavior replacing UserChange

---
+++ src/main/java/co/com/sofka/ramirez/larry/cityairlines/booking/domain/user/UserBehavior.java
@@ -0,0 +1,32 @@
+package co.com.sofka.ramirez.larry.cityairlines.booking.domain.user;
+
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.generic.EventChange;
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.generic.Identity;
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.user.events.CreatedUser;
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.user.values.UserData;
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.user.values.UserFacturationData;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class UserBehavior extends EventChange {
+
+    public UserBehavior(User user) {
+        apply((CreatedUser event) -> {
+           user.facturationData = new UserFacturationData(
+                   event.facturationData().value().street(),
+                   event.facturationData().value().city(),
+                   event.facturationData().value().country(),
+                   event.facturationData().value().zipCode()
+           );
+           user.data = new UserData(
+                   event.data().value().name(),
+                   event.data().value().email(),
+                   event.data().value().password()
+           );
+           user.passengers = new HashSet<>();
+           user.bookingHistoryId = new BookingHistory(new Identity()).identity();
+           user.luggage = new HashMap<>();
+        });
+    }
+}
