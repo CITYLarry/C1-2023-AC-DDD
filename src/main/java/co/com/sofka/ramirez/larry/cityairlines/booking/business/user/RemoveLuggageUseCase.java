ed552b5c77dc54058505ebd9fbea59e4bc945347
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-03-01 18:40:08 -0500

    Define the use case for add luggage to a user

---
+++ src/main/java/co/com/sofka/ramirez/larry/cityairlines/booking/business/user/RemoveLuggageUseCase.java
@@ -0,0 +1,30 @@
+package co.com.sofka.ramirez.larry.cityairlines.booking.business.user;
+
+import co.com.sofka.ramirez.larry.cityairlines.booking.business.commons.CommandUseCase;
+import co.com.sofka.ramirez.larry.cityairlines.booking.business.commons.EventsRepository;
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.generic.DomainEvent;
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.user.User;
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.user.commands.RemoveLuggageCommand;
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.user.values.identities.PassengerId;
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.user.values.identities.UserId;
+
+import java.util.List;
+
+public class RemoveLuggageUseCase implements CommandUseCase<RemoveLuggageCommand> {
+
+    private final EventsRepository eventsRepository;
+
+    public RemoveLuggageUseCase(EventsRepository eventsRepository) {
+        this.eventsRepository = eventsRepository;
+    }
+
+    @Override
+    public List<DomainEvent> apply(RemoveLuggageCommand command) {
+        List<DomainEvent> userEvents = eventsRepository.findAggregateRootId(command.getUserId());
+        User user = User.from(UserId.of(command.getUserId()), userEvents);
+        user.removeLuggage(PassengerId.of(command.getPassengerId()));
+        return user.getUncommittedChanges().stream()
+                .map(eventsRepository::saveEvent)
+                .toList();
+    }
+}
---
+++ src/main/java/co/com/sofka/ramirez/larry/cityairlines/booking/domain/user/commands/RemoveLuggageCommand.java
@@ -0,0 +1,32 @@
+package co.com.sofka.ramirez.larry.cityairlines.booking.domain.user.commands;
+
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.generic.Command;
+
+public class RemoveLuggageCommand extends Command {
+
+    private String passengerId;
+    private String userId;
+
+    public RemoveLuggageCommand(){}
+
+    public RemoveLuggageCommand(String passengerId, String userId) {
+        this.passengerId = passengerId;
+        this.userId = userId;
+    }
+
+    public String getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(String passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
