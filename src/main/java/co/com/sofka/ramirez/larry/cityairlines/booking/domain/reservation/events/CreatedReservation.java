811ea6db0182bea2b9cd6961907fe2c61329fa83
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-03-01 09:01:51 -0500

    Quick fixes

--- src/main/java/co/com/sofka/ramirez/larry/cityairlines/booking/domain/reservation/events/CreatedReservation.java
+++ src/main/java/co/com/sofka/ramirez/larry/cityairlines/booking/domain/reservation/events/CreatedReservation.java
@@ -1,18 +1,25 @@
 package co.com.sofka.ramirez.larry.cityairlines.booking.domain.reservation.events;
 
 import co.com.sofka.ramirez.larry.cityairlines.booking.domain.generic.DomainEvent;
+import co.com.sofka.ramirez.larry.cityairlines.booking.domain.generic.Identity;
 import co.com.sofka.ramirez.larry.cityairlines.booking.domain.reservation.Flight;
 import co.com.sofka.ramirez.larry.cityairlines.booking.domain.reservation.Payment;
 
 public class CreatedReservation extends DomainEvent {
 
+    private final Identity userId;
     private final Flight flight;
     private final Payment payment;
 
-    public CreatedReservation(Flight flight, Payment payment){
+    public CreatedReservation(Identity userId, Flight flight, Payment payment){
         super("co.com.sofka.ramirez.larry.cityairlines.booking.domain.reservation.events.CreatedReservation");
+        this.userId = userId;
         this.flight = flight;
         this.payment = payment;
+    }
+
+    public Identity userId(){
+        return this.userId;
     }
 
     public Flight flight(){
